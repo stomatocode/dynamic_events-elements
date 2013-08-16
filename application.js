640aa3690151c4cb0be0e7e4c7fa27bda6bd658a
DBC Apprentice <apprentice@devbootcamp.com>
2013-08-15 19:31:17 -0700

    append todo as DOM element with template

--- application.js
+++ application.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    $('.add').on('click', function(){
+      var content = $('input').val();
+      var copy = buildTodo(content);
+      $('.todo_list').append(copy);
+    });
+
   }
 
   //Create functions to add, remove and complete todos
@@ -16,6 +22,7 @@ $(document).ready(function() {
     // Modifies it's text to use the passed in todoName.
     $todo.find('h2').text(todoName);
     // Returns the jQueryDOMElement to be used elsewhere.
+    debugger
     return $todo;
   }
   
